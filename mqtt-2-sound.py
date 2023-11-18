e6bf64e542339b324670a23d7bf59dc76a10b730
sordfish <sordfish2050@gmail.com>
2023-11-18 14:04:09 +0000

    update for python3 and add dahuaVTO button topic

--- mqtt-2-sound.py
+++ mqtt-2-sound.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python
-import mosquitto
+#!/usr/bin/python3
+from paho.mqtt import client as mqtt
 import yaml
 import pygame
 import os
@@ -14,8 +14,8 @@ pygame.mixer.init(44100, -16, 1, 2048)
 currently_playing_file = ""
 
 
-def on_message(obj, msg):
-    print "Received %s on topic %s" % (msg.payload, msg.topic)
+def on_message(obj, user, msg):
+    print ("Received %s on topic %s" % (msg.payload, msg.topic))
     if msg.topic == 'door/inner/opened/username':
         # Set volume to 50% for this clip
         play("audio/%s_announce.ogg" % msg.payload, 0.5)
@@ -25,27 +25,32 @@ def on_message(obj, msg):
         play("audio/outer_door_opened.ogg")
     elif msg.topic == 'door/inner/doorbell':
         play("audio/doorbell.ogg")
+    elif msg.topic == 'DahuaVTO/BackKeyLight/Event':
+        play("audio/doorbell.ogg")        
 
 
 def play(filename,level = 1.0):
     global currently_playing_file
     if os.path.isfile(filename):
         if (not pygame.mixer.music.get_busy()) or (currently_playing_file is not filename):
-            print "Playing %s" % filename
+            print ("Playing %s" % filename)
             currently_playing_file = filename
             pygame.mixer.music.load(filename)
             pygame.mixer.music.set_volume(level)
             pygame.mixer.music.play()
 
 
-mqttc = mosquitto.Mosquitto(config['mqtt']['name'])
-mqttc.connect(config['mqtt']['server'], 1883, 60, True)
+mqttc = mqtt.Client(config['mqtt']['name'])
+mqttc.connect(config['mqtt']['server'], 1883, 60)
 
 mqttc.subscribe("door/outer/buzzer")
 mqttc.subscribe("door/outer/opened/username")
 
 mqttc.subscribe("door/inner/doorbell")
 mqttc.subscribe("door/inner/opened/username")
+
+mqttc.subscribe("DahuaVTO/BackKeyLight/Event")
+
 mqttc.on_message = on_message
 
 while mqttc.loop(timeout=100) == 0:
